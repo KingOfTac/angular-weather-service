6474c2370d6a702d183c0ca0724a484620b8180d
Nathan Brown <32497422+KingOfTac@users.noreply.github.com>
2022-01-12 10:21:17 -0700

    fix api endpoint for ssl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  weatherEndpoint: 'http://api.weatherapi.com/v1',
+  weatherEndpoint: 'https://api.weatherapi.com/v1',
   weatherKey: 'f1f9bd10d71d4cc0be504903220601'
 };
